3b44639f526afb064b12cdd19dc8ca8d790097eb
asultan123 <asultan@aucegypt.edu>
2021-09-26 21:28:41 -0400

    Started access map asbtract representation generation

--- StreamLang.py
+++ StreamLang.py
@@ -294,6 +294,10 @@ class ISLGeneratorPreprocessor:
 
 class ISLGenerator:
 
+    @classmethod
+    def generate_concrete_repr(ir: IslIR):
+        it_dom = ir.iteration_domain
+        it_dom = IterationDomain.generate_abstract_repr(ir.name, it_dom) 
 
     @classmethod
     def generate_concrete_repr(ir):
