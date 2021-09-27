863cb7c9d785003507cef823224be642da5dc5b4
asultan123 <asultan@aucegypt.edu>
2021-09-26 21:25:44 -0400

    Moved abstract iteration domain repr to iteration dom class

--- StreamLang.py
+++ StreamLang.py
@@ -158,6 +158,39 @@ class IterationDomain:
         self._vector = vector
 
 
+    @classmethod
+    def generate_abstract_repr(cls, name, iteration_domain):
+        params = ISLGeneratorPreprocessor.preprocess_iterable(iteration_domain.parameters)
+        it_vector = ISLGeneratorPreprocessor.preprocess_iterable(iteration_domain.vector)
+
+        bound_tuples = list(zip(iteration_domain.vector, iteration_domain.bounds))
+        bound_tuples_count = len(bound_tuples)
+
+        bound_expr = ""
+        for idx, (iterator, bound) in enumerate(bound_tuples):
+            # upper bound
+            if len(bound) == 1:
+                bound_expr += f" 0 <= {iterator} < {bound[0]} "
+            # upper and lower bound
+            elif len(bound) == 2:
+                bound_expr += f" {bound[0]} <= {iterator} < {bound[1]} "
+            else:
+                raise SyntaxError("Invalid number of iterator bounds")
+
+            if idx < bound_tuples_count - 1:
+                bound_expr += "and"
+
+        step_adjustment = ""
+        for idx, step in enumerate(iteration_domain.steps):
+            step_adjustment += "and"
+            step_adjustment += f" {iteration_domain.vector[idx]} mod {step} = 0 "
+
+        structure = f"[{params}] -> {{ {name.upper()}[{it_vector}] : {bound_expr} {step_adjustment}}}"
+
+        return isl.BasicSet(structure)
+
+
+
 @dataclass
 class AccessMap:
     access_expr: str = ""
@@ -261,36 +294,6 @@ class ISLGeneratorPreprocessor:
 
 class ISLGenerator:
 
-    @classmethod
-    def generate_abstract_repr(cls, ir: IslIR):
-        params = ISLGeneratorPreprocessor.preprocess_iterable(ir.iteration_domain.parameters)
-        it_vector = ISLGeneratorPreprocessor.preprocess_iterable(ir.iteration_domain.vector)
-
-        bound_tuples = list(zip(ir.iteration_domain.vector, ir.iteration_domain.bounds))
-        bound_tuples_count = len(bound_tuples)
-
-        bound_expr = ""
-        for idx, (iterator, bound) in enumerate(bound_tuples):
-            # upper bound
-            if len(bound) == 1:
-                bound_expr += f" 0 <= {iterator} < {bound[0]} "
-            # upper and lower bound
-            elif len(bound) == 2:
-                bound_expr += f" {bound[0]} <= {iterator} < {bound[1]} "
-            else:
-                raise SyntaxError("Invalid number of iterator bounds")
-
-            if idx < bound_tuples_count - 1:
-                bound_expr += "and"
-
-        step_adjustment = ""
-        for idx, step in enumerate(ir.iteration_domain.steps):
-            step_adjustment += "and"
-            step_adjustment += f" {ir.iteration_domain.vector[idx]} mod {step} = 0 "
-
-        structure = f"[{params}] -> {{ {ir.name.upper()}[{it_vector}] : {bound_expr} {step_adjustment}}}"
-
-        return isl.BasicSet(structure)
 
     @classmethod
     def generate_concrete_repr(ir):
